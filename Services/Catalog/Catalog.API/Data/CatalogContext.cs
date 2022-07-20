6ee69c97b543470ac0a485af1b28403c02b5393f
Emogibam <88818922+Emogibam@users.noreply.github.com>
2022-07-20 04:14:38 +0100

    readme update

--- Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -7,11 +7,18 @@ namespace Catalog.API.Data
 {
     public class CatalogContext : ICatalogContext
     {
+        /// <summary>
+        /// Creating a database while instantiating the constructor
+        /// </summary>
+        /// <param name="configuration"></param>
         public CatalogContext(IConfiguration configuration)
         {            
+            // creating a mongodb client
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            //creatind a database and if there is no database it creates a new one
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
+            // getting a mongobd collection from the the appsetting, the collection name is product
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
             CatalogContextSeed.SeedData(Products);
         }
